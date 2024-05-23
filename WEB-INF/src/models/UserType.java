19c08cab961ac66ccbf5f715320d0bbd89d9fb5e
Aryan-219 <aryanagr219@gmail.com>
2024-05-23 19:15:39 +0530

    D14: Optimization

--- WEB-INF/src/models/UserType.java
+++ WEB-INF/src/models/UserType.java
@@ -1,10 +1,5 @@
 package models;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.ArrayList;
-
 public class UserType {
     // ################### Properties #########################
     private Integer userTypeId;
